1d6ce83d15eabc17898aea30691c01d96e0adad4
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-05 16:39:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	var mapping = {
+		  2: "abc",
+		  3: "def",
+		  4: "ghi",
+		  5: "jkl",
+		  6: "mno",
+		  7: "pqrs",
+		  8: "tuv",
+		  9: "wxyz",
+		  0: "0",
+		  1: "1"
+	};
+
+	var stringArray = [''];
+
+	for(let i = 0; i < input_digit.length; i++){
+		let digit = input_digit[i];
+		let letters = mapping[digit];
+		let newCombination = [];
+
+		for(let j = 0; j < stringArray.length; j++){
+			var combination = stringArray[j];
+
+			for(let k = 0; k < letters.length; k++){
+				newCombination.push(combination + letters[k]);
+			}
+		}
+		stringArray = newCombination;
+	}
+	return stringArray;
 }
 
 module.exports = letterCombinations;
